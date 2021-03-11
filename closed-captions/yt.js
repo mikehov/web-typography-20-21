c22a65fb26f7c644e9285cab3b0dd97f7fcf03c2
Mike <mikehovje@gmail.com>
2021-03-11 16:37:20 +0100

    Version 2
    Update till 1:09

--- closed-captions/yt.js
+++ closed-captions/yt.js
@@ -2,8 +2,6 @@ var player;
 
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('video-placeholder', {
-		width: 600,
-		height: 400,
 		videoId: 'vrP-_T-h9YM',
 		playerVars: {
 			color: 'white',
